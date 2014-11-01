dcd25e976cfa67958892682e5766ad966d09f05b
Pawel Mikolajczyk <pawel.b.mikolajczyk@gmail.com>
2014-11-01 20:25:25 +0100

    add blank my_stuff module sample

---
+++ my_stuff_BLANK.py
@@ -0,0 +1,10 @@
+from msi import AbstractMsiDataProvider
+
+
+class YourCoolMsiDataProvider(AbstractMsiDataProvider):
+    def __init__(self):
+        pass
+
+def thingsYouDoWithTheNeuralNetwork(network, datasetForTraining, datasetForTest):
+    # write your fancy scientific experiment below
+    pass
